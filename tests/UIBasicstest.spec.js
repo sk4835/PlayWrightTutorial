db059e07947d87bfc4ec125404cd216d2b6c22c2
Shahid Khan <shahid4835@gmail.com>
2024-08-28 17:19:40 -0400

    Comment explaination

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,14 @@
+const {test} = require('@playwright/test');
+
+
+test('First Playwright test', async ()=> {
+
+//playwright code can be written here
+//Step 1 - open browser
+// having the 'await' keyword after every step to ensure steps are properly complete before the next step is executed 
+//also 'async' function is requried for await to function accordingly
+//Step 2 - enter U/P (takes 2 seconds)
+//Step 3 - click
+// JS is asychronus meaning no gaurentee it will be executed step by step but rather will be executed at the same time
+
+});
