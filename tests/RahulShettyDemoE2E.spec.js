6cd54f1c48dc068a091749b8433d1ed28832e8e3
Shahid Khan <shahid4835@gmail.com>
2024-09-25 19:11:13 -0400

    waitFor methods in appropriate location

--- tests/RahulShettyDemoE2E.spec.js
+++ tests/RahulShettyDemoE2E.spec.js
@@ -12,8 +12,8 @@ const { escape } = require('querystring');
         const passField = page.locator("#userPassword")
         const loginButton = page.locator("#login")
         const products = page.locator(".card-body");
-        const addToCartBtn = page.locator("[class='btn w-10 rounded']");
         const productName = "ZARA COAT 3"
+        const naviButton = page.locator("[class='btn btn-custom']");
 
         await page.goto("https://rahulshettyacademy.com/client");
         await userName.fill(email);
@@ -21,23 +21,27 @@ const { escape } = require('querystring');
         await loginButton.click();
         //console.log(await page.title());
         //expect(await page.getByTitle("Let's Shop"));
-        await page.pause();
+        //await page.pause();
+        
+        await products.first().waitFor();
         const count = await products.count();
         for (let i = 0; i < count; i++)
-        {
+        {    
             if(await products.nth(i).locator("b").textContent() === productName)
             {
                 await products.nth(i).locator("text= Add To Cart").click();
+                await naviButton.nth(2).locator("label").waitFor();
+                expect(await naviButton.nth(2).locator("label")).toHaveText("1");
                 console.log("SUCCESS")
-                console.log(await page.locator("[class='btn btn-custom']").nth(2).locator("label").textContent());
-                expect(await page.locator("[class='btn btn-custom']").nth(2).locator("label")).toHaveText("1");
+                console.log(await naviButton.nth(2).locator("label").textContent());
                 break;
             }
         }
+        await naviButton.nth(2).click();
+        expect(await page.locator(".heading h1")).toHaveText("My Cart");
+        console.log(await page.locator(".heading h1").textContent());
 
         
 
-        
-        
      });
 
