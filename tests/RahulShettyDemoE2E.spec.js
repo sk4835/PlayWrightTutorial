4ae1e7f221247b62f1a5fb3d26d3e337cf4d8c83
Shahid Khan <shahid4835@gmail.com>
2024-09-25 18:32:17 -0400

    E2E test: added validation for add to cart

--- tests/RahulShettyDemoE2E.spec.js
+++ tests/RahulShettyDemoE2E.spec.js
@@ -1,12 +1,42 @@
 const {test, expect} = require('@playwright/test');
 const { timeout } = require('../playwright.config');
+const playwrightConfig = require('../playwright.config');
+const { escape } = require('querystring');
 
 
     test.only('@Web Client App login', async ({ page }) => {
         //js file- Login js, DashboardPage
         const email = "anshika@gmail.com";
         const password = "Iamking@000"
-        // to automate from E2E on this site: https://rahulshettyacademy.com/client
+        const userName = page.locator("#userEmail")
+        const passField = page.locator("#userPassword")
+        const loginButton = page.locator("#login")
+        const products = page.locator(".card-body");
+        const addToCartBtn = page.locator("[class='btn w-10 rounded']");
+        const productName = "ZARA COAT 3"
+
+        await page.goto("https://rahulshettyacademy.com/client");
+        await userName.fill(email);
+        await passField.fill(password);
+        await loginButton.click();
+        //console.log(await page.title());
+        //expect(await page.getByTitle("Let's Shop"));
+        await page.pause();
+        const count = await products.count();
+        for (let i = 0; i < count; i++)
+        {
+            if(await products.nth(i).locator("b").textContent() === productName)
+            {
+                await products.nth(i).locator("text= Add To Cart").click();
+                console.log("SUCCESS")
+                console.log(await page.locator("[class='btn btn-custom']").nth(2).locator("label").textContent());
+                expect(await page.locator("[class='btn btn-custom']").nth(2).locator("label")).toHaveText("1");
+                break;
+            }
+        }
+
+        
+
         
         
      });
